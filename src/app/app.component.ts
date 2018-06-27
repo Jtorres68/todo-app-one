649e50d179367c684d55d0de5db1faacbc881e7b
Jtorre68 <jtorre68@fau.edu>
2018-06-27 18:07:46 -0400

    dhfh

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import {Component} from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
+})
+export class AppComponent {
+
+  newTodo: Todo = new Todo();
+  
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+  
+  updateTodo(todo, newValue) {
+  todo.title = newValue;
+  return this.todoDataService.put(todo).then(() => {
+    todo.editing = false;
+    return this.todoDataService.getAllTodos();
+  });
+}
+
+}
