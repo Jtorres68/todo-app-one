795427d59556dea0b94c2a58ca40b55b414a400d
Jtorre68 <jtorre68@fau.edu>
2018-06-30 02:24:57 -0400

    part-4a

--- src/app/todos/todos.component.spec.ts
+++ src/app/todos/todos.component.spec.ts
@@ -1,38 +1,41 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { TodosComponent } from './todos.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ApiMockService } from '../api-mock.service';
+import { TodoDataService } from '../todo-data.service';
+import { ApiService } from '../api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
 
-beforeEach(async(() => {
-  TestBed.configureTestingModule({
-    declarations: [TodosComponent],
-    schemas: [
-      NO_ERRORS_SCHEMA
-    ],
-    providers: [
-      TodoDataService,
-      {
-        provide: ApiService,
-        useClass: ApiMockService
-      },
-      {
-        provide: ActivatedRoute,
-        useValue: {
-          data: Observable.of({
-            todos: []
-          })
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TodosComponent],
+      providers: [
+        TodoDataService,
+        {
+          provide: ApiService,
+          useClass: ApiMockService
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            data: Observable.of({
+              todos: []
+            })
+          }
         }
-      }
-    ],
-  })
-    .compileComponents();
-}));
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
+    })
+      .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosComponent);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-
-  // URL of production API
   apiUrl: 'http://localhost:3000'
 };
